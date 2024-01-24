2a211afa3ef775098c03ab4803c9a188e5c65d33
hanxiong li <hanxiong.li98@gmail.com>
2024-01-24 18:14:30 +0100

    Revert "exe3"
    This reverts commit 28c091df3c0dad7158d2731c928f26d12894b626.

--- lie_group/Lie_funcs_matlab/cost_func.m
+++
@@ -1,3 +0,0 @@
-function res = cost_func(rotv,x,y)
-C = expm(v2skew(rotv)); % rotation matrix
-res = squared_distance(y, C*x);
--- lie_group/Lie_funcs_matlab/squared_distance.m
+++
@@ -1,3 +0,0 @@
-function res = squared_distance(x,y)
-num_points = size(x,2);
-res = sum(sum((x - y).^2)) / num_points;
